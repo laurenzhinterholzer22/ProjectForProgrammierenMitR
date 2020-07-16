eefb0529b055ad83b3324470aa1c0515a716a9d7
ManuelKarlsberger <manuel.karlsberger@gmx.at>
2020-07-16 14:42:44 +0200

    small changes und 5 großteils

--- ProjectAufgabe1.R
+++ ProjectAufgabe1.R
@@ -114,7 +114,7 @@ hist(yearLsoa$area_sq_km, main = "Lsoa Flaeche",xlab = "Flaeche", ylab = "Haeufi
 install.packages("sf")
 library(sf)
 boundariesLondon <- st_read("LSOA_2011_London_gen_MHW.shp")
-summary(boundariesLondon)
+
 class(boundariesLondon)
 dim(boundariesLondon)
 colnames(boundariesLondon)
@@ -182,35 +182,25 @@ energyTot <- yearMsoa$energy_tot
 energyProteinTot <- energySugarTot <- energyCarbTot <- energySatTot <- energyFibreTot <-  energyFatTot <- numeric(length(nrow(yearMsoa)))
 
 
-for(i in seq_along(length(energyTot))) {
   #energyFatTot <- (energyTot - energyFat) / energyTot
-  energyFatTot <- energyFat / energyTot
-}
+energyFatTot <- energyFat / energyTot
 
-for(i in seq_along(length(energyTot))) {
   #energyFibreTot <- (energyTot - energyFibre) / energyTot
-  energyFibreTot <- energyFibre / energyTot
-}
+energyFibreTot <- energyFibre / energyTot
 
-for(i in seq_along(length(energyTot))) {
   #energySatTot <- (energyTot - energySat) / energyTot
-  energySatTot <- energySat / energyTot
-}
+energySatTot <- energySat / energyTot
 
-for(i in seq_along(length(energyTot))) {
+
   #energyCarbTot <- (energyTot - energyCarb) / energyTot
-  energyCarbTot <- energyCarb / energyTot
-}
+energyCarbTot <- energyCarb / energyTot
 
-for(i in seq_along(length(energyTot))) {
   #energySugarTot <- (energyTot - energySugar) / energyTot
-  energySugarTot <- energySugar / energyTot
-}
+energySugarTot <- energySugar / energyTot
 
-for(i in seq_along(length(energyTot))) {
-  #energyProteinTot <- (energyTot - energyProtein) / energyTot
-  energyProteinTot <- energyProtein / energyTot
-}
+#energyProteinTot <- (energyTot - energyProtein) / energyTot
+energyProteinTot <- energyProtein / energyTot
+
 
 par(mfrow=c(2,3))
 hist(energyCarbTot, main = NULL, xlab = "Energie von Kohlenhydraten", ylab = "Haeufigkeit",breaks=seq(0,0.6,length=100), col = "red", ylim = c(0,300))
@@ -220,4 +210,79 @@ hist(energySugarTot, main = NULL, xlab = "Energie von Zucker", ylab = "Haeufigke
 hist(energyProteinTot, main = NULL, xlab = "Energie von Eiweiß", ylab = "Haeufigkeit",breaks=seq(0,0.6,length=100), col = "green", ylim = c(0,300))
 hist(energyFibreTot,main = NULL, xlab = "Energie von Ballaststoffe", ylab = "Haeufigkeit",breaks=seq(0,0.6,length=100), col = "lightgreen", ylim = c(0,300))
 
+
+
+#5
+
+diabetes <- read.table("diabetes_estimates_osward_2016.csv", header = TRUE, sep = ",")
+head(diabetes)
+
+
+londonWardX <- st_read("./statistical-gis-boundaries-london/statistical-gis-boundaries-london/ESRI/London_Ward_CityMerged.shp")
+londonWardY <- st_read("./London-wards-2014/London-wards-2014 (1)/London-wards-2014_ESRI/London_Ward_CityMerged.shp")
+
+dim(londonWardX)
+dim(diabetes)
+dim(londonWardY)
+merged <- st_join(diabets, londonWardY)
+merged <- merge(diabetes, yearOsward, by.x = "area_id", by.y = "area_id")
+dim(merged)
+head(merged)
+
+FUN <- function(var.x, var.y, ...){
+  
+  
+}
+class(londonWardX)[1] == "sf"
+
+compare_data <- function(x, y, id.x, id.y = id.x, var.x = character(0), var.y = var.x, FUN = sum, ...) {
+  if(!is.list(x) | !is.list(y))
+    stop("Bitte überprüfen Sie Ihre Eingabe für x und y!")
+  if(!is.character(id.x) | (!is.character(id.y) & !is.na(id.y)))
+    stop("es muss eine korrekte ID angegeben werden!")
+  if(!is.character(var.x) | (!is.character(var.y) & !is.na(var.y)))
+    stop("es muss eine korrekte Variable angegeben werden!")
+  
+  if(class(x)[1] == "sf" & class(y)[1] == "sf"){
+    #spezielle Behandlung bei merge von 2 sf Objekten (vllt st_join)
+    mergedXandY <- merge(x %>% as.data.frame(), y %>% as.data.frame(), by.x = id.x, by.y = id.y)
+    mergedXandY %>% st_sf(sf_column_name = 'geometry.x')
+    mergedX <- merge(x %>% as.data.frame(), y %>% as.data.frame(), by.x = id.x, by.y = id.y, all.x = TRUE)
+    mergedX %>% st_sf(sf_column_name = 'geometry.x')
+    mergedY <- merge(x %>% as.data.frame(), y %>% as.data.frame(), by.x = id.x, by.y = id.y, all.y = TRUE)
+    mergedY %>% st_sf(sf_column_name = 'geometry.y')
+    
+    sumXandY <- (FUN(get(paste(var.x, ".x",sep = ""), mergedXandY), ...))
+    sumX <- (FUN(get(paste(var.x,".x",sep = ""), mergedX), ...))
+    sumY <- (FUN(get(paste(var.x,".y",sep = ""), mergedY),...))
+  }
+  else{
+    mergedXandY <- merge(x, y, by.x = id.x, by.y = id.y)
+    mergedX <- merge(x, y, by.x = id.x, by.y = id.y, all.x = TRUE)
+    mergedY <- merge(x, y, by.x = id.x, by.y = id.y, all.y = TRUE)
+    
+    sumXandY <- (FUN(get(var.x, mergedXandY), ...))
+    sumX <- (FUN(get(var.x, mergedX), ...))
+    sumY <- (FUN(get(var.x, mergedY),...))
+  }
+  
+  
+  df <- data.frame(mergeType = c("X und Y", "nur X", "nur Y"), 
+                   anzahl = c(nrow(mergedXandY),nrow(mergedX)-nrow(mergedXandY), nrow(mergedY)-nrow(mergedXandY)),
+                   hektar = c(round(sumXandY,0), round(sumX-sumXandY,0), round(sumY-sumXandY,0)))
+  
+  if(var.x != var.y){
+    popXandY <- (FUN(get(var.y, mergedXandY), ...))
+    popX <- (FUN(get(var.y, mergedX), ...))
+    popY <- (FUN(get(var.y, mergedY), ...))
+    df$hektar <- df$hektar*100
+    df <- cbind(df, bevölkerung = c(popXandY, popX-popXandY, popY-popXandY))
+  }
+ 
+  return(df)
+}
+
+compare_data(londonWardX, londonWardY, "GSS_CODE", "GSS_CODE", "HECTARES", "HECTARES", na.rm = TRUE)
+compare_data(diabetes, londonWardY, "area_id", "GSS_CODE", "HECTARES", na.rm= TRUE)
+compare_data(diabetes, yearOsward, "area_id", "area_id", "area_sq_km", "population", na.rm = TRUE)
 
